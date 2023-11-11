9b0db3330fe4946aa7d8109e92ba996abb271186
Andrea Olivos <andrea.olivos.b@gmail.com>
2023-11-11 19:42:59 -0300

    Remove pills, add email button

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,17 @@
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "@nextui-org/react";
 import Image from "next/image";
 
 const HomePage = () => {
   return (
     <main className='flex flex-col justify-center align-middle text-center py-16 lg:px-48 px-12'>
       <Image src="/photo.webp" width="200" height="200" alt="Fondo lila claro con constelaciones blancas" className='m-auto rounded-full border-4 border-purple-300' />
-      <h1 className='text-blue-950 text-3xl font-bold my-8'>Hola, soy Andrea, desarrolladora web full stack :)</h1>
-      <div className='text-blue-950 space-x-4 space-y-4 lg:px-48 xl:px-60'>
-        <span className='bg-pink-300 rounded-full px-4 py-3 inline-block'>HTML</span>
-        <span className='bg-purple-300 rounded-full px-4 py-3 inline-block'>CSS</span>
-        <span className='bg-pink-300 rounded-full px-4 py-3 inline-block'>Javascript</span>
-        <span className='bg-purple-300 rounded-full px-4 py-3 inline-block'>React</span>
-        <span className='bg-pink-300 rounded-full px-4 py-3 inline-block'>NodeJS</span>
-        <span className='bg-purple-300 rounded-full px-4 py-3 inline-block'>Express</span>
-        <span className='bg-pink-300 rounded-full px-4 py-3 inline-block'>MongoDB</span>
-        <span className='bg-purple-300 rounded-full px-4 py-3 inline-block'>Bootstrap</span>
-        <span className='bg-pink-300 rounded-full px-4 py-3 inline-block'>Tailwind</span>
-      </div>
+      <h1 className='text-blue-950 text-4xl font-bold my-8'>Hola, soy Andrea, desarrolladora web full stack :)</h1>
+      <p className="text-blue-950 text-xl">Vivo en Talca, Chile, y me dedico a crear sitios web bonitos y funcionales.</p>
+      <span className="mt-10 cursor-pointer">
+        <Link className="bg-purple-300 text-blue-950 text-lg font-bold px-6 py-4 rounded-full" href="mailto:andrea.olivos.b@gmail.com"><FontAwesomeIcon icon={faEnvelope} className="me-2" />Contáctame</Link>
+      </span>
     </main>
   )
 }
