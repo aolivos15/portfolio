73bca0c61a06abed7812bbb364ed7cc356fdf793
Andrea Olivos <andrea.olivos.b@gmail.com>
2023-11-12 19:29:30 -0300

    Add border to contact button

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,10 +7,10 @@ const HomePage = () => {
   return (
     <main className='flex flex-col justify-center align-middle text-center py-16 lg:px-48 px-12'>
       <Image src="/photo.webp" width="200" height="200" alt="Andrea Olivos" className='m-auto rounded-full border-4 border-purple-300' />
-      <h1 className='text-blue-950 text-4xl font-bold my-8'>Hola, soy Andrea, desarrolladora web full stack :)</h1>
+      <h1 className='text-blue-950 text-4xl font-bold my-8'>Hola, soy Andrea Olivos, desarrolladora web full stack :)</h1>
       <p className="text-blue-950 text-xl">Vivo en Talca, Chile, y me dedico a crear sitios web bonitos y funcionales.</p>
       <span className="mt-10 cursor-pointer">
-        <Link className="bg-purple-300 text-blue-950 text-lg font-bold px-6 py-4 rounded-full" href="mailto:andrea.olivos.b@gmail.com"><FontAwesomeIcon icon={faEnvelope} className="me-2" />Contáctame</Link>
+        <Link className="bg-purple-300 text-blue-950 text-lg font-bold px-6 py-4 rounded-full border-2 border-dashed border-purple-500 drop-shadow-md" href="mailto:andrea.olivos.b@gmail.com"><FontAwesomeIcon icon={faEnvelope} className="me-2" />Contáctame</Link>
       </span>
     </main>
   )
